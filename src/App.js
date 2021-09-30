4278c289d99f253c4d147060c72ad35860d1e8d9
shutsuchi <wwww.www.wshu@gmail.com>
2021-09-30 22:40:47 +0900

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
